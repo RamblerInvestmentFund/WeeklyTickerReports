11876ea982282d65829c20435f2ddd3042899871
anthonypeters <apeters10@luc.edu>
2021-10-19 15:38:20 -0500

    Tear sheet for individual ticker using quant stats

--- bot.py
+++ bot.py
@@ -5,48 +5,54 @@ import urllib.request
 import os
 import requests
 from finviz import Screener
+import pdfkit
+import quantstats as qs
 
-stock = Screener(tickers=['AAPL'])
-#stock.get_charts(period="d", size="l", chart_type="c", ta="1")
+#stock = Screener(tickers=['AAPL'])
 
-today = datetime.date.today()
-# ---- Cover Page
-pdf = FPDF(orientation='landscape')
-pdf.set_margin(2)
-pdf.add_page()
-pdf.set_fill_color(128, 0, 0)
-pdf.rect(x = 0, y = 0, w = pdf.w, h = 12, style = 'F')
-# ---- Date
-pdf.set_font("Times", size=22)
-pdf.set_font(style='B')
-pdf.set_text_color(237, 232, 228)
-pdf.cell(txt=str(today), align='C')
-# ----
-pdf.set_fill_color(128, 0, 0)
-pdf.rect(x = 0, y = 198, w = pdf.w, h = 12, style = 'F')
-pdf.image(x = -0.5, y = 12, w = pdf.w + 1, name='RIFLogo(B&W).png')
+#qs.extend_pandas()
+#stock = qs.utils.download_returns("AAPL")
+#qs.reports.html(returns = stock, benchmark = "SPY", output='aapl.html')
+pdfkit.from_file('aapl.html', 'aapl_1.pdf')
 
-# ---- Second Page
-pdf.add_page()
-pdf.set_fill_color(128, 0, 0)
-pdf.rect(x = 0, y = 0, w = pdf.w, h = 12, style = 'F')
-pdf.rect(x = 0, y = 198, w = pdf.w, h = 12, style = 'F')
-pdf.set_fill_color(0, 0, 0)
-pdf.rect(x = 0, y = 12, w = pdf.w, h = 186, style = 'F')
-pdf.image(x=1, y=13, w=(pdf.w+1)/2, name='./charts/AAPL_1632970177.png')
+# today = datetime.date.today()
+# # ---- Cover Page
+# pdf = FPDF(orientation='landscape')
+# pdf.set_margin(2)
+# pdf.add_page()
+# pdf.set_fill_color(128, 0, 0)
+# pdf.rect(x = 0, y = 0, w = pdf.w, h = 12, style = 'F')
+# # ---- Date
+# pdf.set_font("Times", size=22)
+# pdf.set_font(style='B')
+# pdf.set_text_color(237, 232, 228)
+# pdf.cell(txt=str(today), align='C')
+# # ----
+# pdf.set_fill_color(128, 0, 0)
+# pdf.rect(x = 0, y = 198, w = pdf.w, h = 12, style = 'F')
+# pdf.image(x = -0.5, y = 12, w = pdf.w + 1, name='RIFLogo(B&W).png')
 
-# ---- Third Page
-pdf.add_page()
-pdf.set_fill_color(128, 0, 0)
-pdf.rect(x = 0, y = 0, w = pdf.w, h = 12, style = 'F')
-pdf.rect(x = 0, y = 198, w = pdf.w, h = 12, style = 'F')
-pdf.set_fill_color(0, 0, 0)
-pdf.rect(x = 0, y = 12, w = pdf.w + 1, h = 186, style = 'F')
+# # ---- Second Page
+# pdf.add_page()
+# pdf.set_fill_color(128, 0, 0)
+# pdf.rect(x = 0, y = 0, w = pdf.w, h = 12, style = 'F')
+# pdf.rect(x = 0, y = 198, w = pdf.w, h = 12, style = 'F')
+# pdf.set_fill_color(0, 0, 0)
+# pdf.rect(x = 0, y = 12, w = pdf.w, h = 186, style = 'F')
+# pdf.image(x=1, y=13, w=(pdf.w+1)/2, name='./charts/AAPL_1632970177.png')
 
-# ---- PDF Output
-pdf.output('pdf_2.pdf')
+# # ---- Third Page
+# pdf.add_page()
+# pdf.set_fill_color(128, 0, 0)
+# pdf.rect(x = 0, y = 0, w = pdf.w, h = 12, style = 'F')
+# pdf.rect(x = 0, y = 198, w = pdf.w, h = 12, style = 'F')
+# pdf.set_fill_color(0, 0, 0)
+# pdf.rect(x = 0, y = 12, w = pdf.w + 1, h = 186, style = 'F')
+
+# # ---- PDF Output
+# pdf.output('pdf_2.pdf')
 
 
-# ---- Call Bot and Send Portfolio
-# client = slack.WebClient(token = '####')
-# client.files_upload(channels = '#sector-materials', file='./pdf_1.pdf')
+# # ---- Call Bot and Send Portfolio
+# # client = slack.WebClient(token = '####')
+# # client.files_upload(channels = '#sector-materials', file='./pdf_1.pdf')
