0a201c397a0cbe60a914eb2b3e3b15d230b6caa6
nshan651 <nickesg44@gmail.com>
2021-11-09 19:58:30 -0600

    Moved Python code to separately sourced .py file

--- main.py
+++ main.py
@@ -3,166 +3,145 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import re
-import pytz
-import datetime
+import time
 from datetime import date
 
-pytz.utc
 
-def market_profile(ticker):
-    # Declare finviz object
-    stock_info = fv.get_stock(ticker)
-    # Create a mkt profile of select statistics
-    mkt_profile = {
-    'Price':float(stock_info['Price']),
-    'Beta':float(stock_info['Beta']),
-    'Market Cap':str(stock_info['Market Cap']),
-    'P/E':float(stock_info['P/E']),
-    'P/B':float(stock_info['P/B']),
-    'EPS (ttm)':float(stock_info['EPS (ttm)']),
-    'ROE':str(stock_info['ROE']),
-    'ROI':str(stock_info['ROI'])}
+plt.rcParams.update({'font.size': 14})
+plt.xticks(rotation=25)
 
-    # Reticulate variables
-    PRICE = mkt_profile['Price']
-    BETA = mkt_profile['Beta']
-    MARKET_CAP = mkt_profile['Market Cap']
-    P_E = mkt_profile['P/E']
-    P_B = mkt_profile['P/B']
-    EPS = mkt_profile['EPS (ttm)']
-    ROE = str(mkt_profile['ROE'])
-    ROI = str(mkt_profile['ROI'])
-
-    # Remove % sign (Latex doesn't like compiling these for some reason)
-    ROE = ROE[:-1]
-    ROI = ROI[:-1]
-    mkt = pd.DataFrame.from_dict(mkt_profile, orient='index', columns=[''])
-
-def ticker_news(ticker):
-    # Format ticker news
-    news = fv.get_news(ticker)
-
-    story1_date=news[0][0]
-    story1_short=news[0][1]
-    story1_link=news[0][2]
-
-    story2_date=news[1][0]
-    story2_short=news[1][1]
-    story2_link=news[1][2]
-
-    story3_date=news[2][0]
-    story3_short=news[2][1]
-    story3_link=news[2][2]
-
-    story4_date=news[3][0]
-    story4_short=news[3][1]
-    story4_link=news[3][2]
-
-    story5_date=news[3][0]
-    story5_short=news[3][1]
-    story5_link=news[3][2]
-
-
-def price_chart(df, ticker, file_name):
-    # Plot
-    print(df['Close'])
-    fig = plt.plot(df['Date'], df['Close'], color='#800000')
-    plt.title(f'{ticker} Price History')
-    #plt.show(bbox_inches='tight')
-    plt.savefig(file_name, bbox_inches='tight')
-
-
+def price_chart(ticker):
+  stock = yf.Ticker(ticker)
+  hist = stock.history(period='6mo', interval='1d')
+  df = pd.DataFrame(data=hist, columns=['Close'])
+  df.reset_index(inplace=True)
+  
+  # Plot
+  plt.plot(df['Date'], df['Close'], color='#800000')
+  plt.title(f'{ticker} Price History')
+  plt.savefig('./rif-logos/price-chart.png', bbox_inches='tight')
+  #plt.close(fig1)
+  
+# Generic barplot
 def bar_plots(df, x, y, color, title, file_name):
-    fig = plt.figure(figsize = (5, 4))
-    ax = sns.barplot(data=df, x=x, y=y, color=color)
-    sns.despine()
-    plt.bar_label(ax.containers[0])
-    plt.title(title, pad=15)
-    plt.savefig(file_name, bbox_inches='tight')
-
-
+  fig = plt.figure(figsize = (5, 4))
+  ax = sns.barplot(data=df, x=x, y=y, color=color)
+  sns.despine()
+  plt.bar_label(ax.containers[0])
+  plt.title(title, pad=15)
+  plt.savefig(file_name, bbox_inches='tight')
+  plt.close(fig)
+  
 def stacked_bar(df, x, y1, y2, color, title, file_name):
-    fig, ax = plt.subplots(figsize=(5,4))
+  fig, ax = plt.subplots(figsize=(5,4))
+  
+  ax.bar(x, y1, color=color[0], label='Revenue')
+  ax.bar(x, y2, color=color[1], bottom=y1, label='Earnings')
+  
+  ax.spines['right'].set_visible(False)
+  ax.spines['top'].set_visible(False)
+  
+  plt.bar_label(ax.containers[1])
+  ax.set_title(title, pad=15)
+  ax.legend()
+  plt.savefig(file_name, bbox_inches='tight')
+  plt.close(fig)
 
-    ax.bar(x, y1, color=color[0], label='Revenue')
-    ax.bar(x, y2, color=color[1], bottom=y1, label='Earnings')
+### Driver Code ###
 
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    plt.bar_label(ax.containers[1])
-    ax.set_title(title, pad=15)
-    ax.legend()
-    plt.savefig(file_name, bbox_inches='tight')
+TICKER = 'MSFT'
 
+#
+# FINVIZ
+#
 
-def main():
+# Finviz objects
+stock_info = fv.get_stock(TICKER)
+news = fv.get_news(TICKER)
 
-    TICKER = 'MSFT'
+# Reticulate global variables
+PRICE = float(stock_info['Price'])
+BETA = float(stock_info['Beta'])
+MARKET_CAP = str(stock_info['Market Cap'])
+P_E = float(stock_info['P/E'])
+P_B = float(stock_info['P/B'])
+EPS = float(stock_info['EPS (ttm)'])
+# Remove % signs in ROE and ROI
+ROE = str(stock_info['ROE'][:-1])
+ROI = str(stock_info['ROI'][:-1])
 
-    ### Finviz call ###
+# Format ticker news
+STORY1_DATE=news[0][0]
+STORY1_SHORT=news[0][1]
+STORY1_LINK=news[0][2]
 
-    market_profile(TICKER)
+STORY2_DATE=news[1][0]
+STORY2_SHORT=news[1][1]
+STORY2_LINK=news[1][2]
 
-    ### yfinance calls ###
+STORY3_DATE=news[2][0]
+STORY3_SHORT=news[2][1]
+STORY3_LINK=news[2][2]
 
-    stock = yf.Ticker(TICKER)
-    # Stock objects
-    hist = stock.history(period='6mo', interval='1d')
-    fin = stock.financials
-    bal = stock.balance_sheet
-    cash = stock.cashflow
-    earn = stock.earnings
-    splits = stock.splits
-    # Create data frames
-    price_data = pd.DataFrame(data=hist, columns=['Close'])
-    price_data.reset_index(inplace=True)
-    financial_data = pd.DataFrame(data=fin)
-    balance_data = pd.DataFrame(data=bal)
-    cashflow_data = pd.DataFrame(data=cash)
-    earnings_data = pd.DataFrame(data=earn)
+STORY4_DATE=news[3][0]
+STORY4_SHORT=news[3][1]
+STORY4_LINK=news[3][2]
 
-    # Stock Summary and News
-    summary = stock.info['longBusinessSummary']
+STORY5_DATE=news[4][0]
+STORY5_SHORT=news[4][1]
+STORY5_LINK=news[4][2]
 
-    # Date x-axis data
-    #curr_year = date.today().year
-    year = str(datetime.datetime.today().strftime('%Y'))
-    curr_year=int(year)
-    dt = ['{}'.format(curr_year), '{}'.format(curr_year-1), '{}'.format(curr_year-2), '{}'.format(curr_year-3)]
-    
-    # Financials
-    gross = [financial_data.loc['Gross Profit'][i]/1000000 for i in range(4)]
-    ebit = [financial_data.loc['Ebit'][i]/1000000 for i in range(4)]
-    debt_short = [balance_data.loc['Short Long Term Debt'][i]/1000000 for i in range(4)]
-    debt_long = [balance_data.loc['Long Term Debt'][i]/1000000 for i in range(4)]
-    revenue = [earnings_data.iloc[i][0]/1000000 for i in range(4)]
-    earnings = [earnings_data.iloc[i][1]/1000000 for i in range(4)]
+#
+# YFINANCE
+#
 
-    operating_cash = cashflow_data.loc['Total Cash From Operating Activities']
-    capital_expend = cashflow_data.loc['Capital Expenditures']
-    fcf = []
-    # Calculate Free Cash Flows (operating cash - capex)
-    for (op_cash, capex) in zip(operating_cash, capital_expend):
-        fcf.append(op_cash/1000000 - capex/1000000)
+# yfinance objects
+stock = yf.Ticker(TICKER)
+fin = stock.financials
+bal = stock.balance_sheet
+cash = stock.cashflow
+earn = stock.earnings
+splits = stock.splits
 
-    ### Save charts ###
+# Global variables
+SUMMARY = stock.info['longBusinessSummary']
+EXACT_TIME = time.strftime('%H:%M%p %Z on %b %d, %Y')
 
-    # Financial
-    price_chart(price_data, TICKER, './rif-logos/price-chart.png')
-    bar_plots(financial_data, dt, gross, '#FEBD18', 'Gross Profit ($ millions)', './rif-logos/gross-profit.png')
-    bar_plots(financial_data, dt, ebit, '#800000', 'EBIT ($ millions)', './rif-logos/ebit.png')
+# X-axis time series data
+curr_year = int(date.today().year) 
+dt = ['{}'.format(curr_year), '{}'.format(curr_year-1), '{}'.format(curr_year-2), '{}'.format(curr_year-3)]
 
-    # Balance sheet
-    bar_plots(balance_data, dt, debt_short, '#800000', 'Short Long Term Debt ($ millions)', './rif-logos/debt_short.png')
-    bar_plots(balance_data, dt, debt_long, '#FEBD18', 'Long Term Debt ($ millions)', './rif-logos/debt_long.png')
+# Create data frames
+financial_data = pd.DataFrame(data=fin)
+balance_data = pd.DataFrame(data=bal)
+cashflow_data = pd.DataFrame(data=cash)
+earnings_data = pd.DataFrame(data=earn)
 
-    # Cashflow
-    bar_plots(cashflow_data, dt, fcf, '#FEBD18', 'Free Cash Flow ($ millions)', './rif-logos/fcf.png')
+# Financials
+gross = [financial_data.loc['Gross Profit'][i]/1000000 for i in range(4)]
+ebit = [financial_data.loc['Ebit'][i]/1000000 for i in range(4)]
+debt_short = [balance_data.loc['Short Long Term Debt'][i]/1000000 for i in range(4)]
+debt_long = [balance_data.loc['Long Term Debt'][i]/1000000 for i in range(4)]
+revenue = [earnings_data.iloc[i][0]/1000000 for i in range(4)]
+earnings = [earnings_data.iloc[i][1]/1000000 for i in range(4)]
+# Derive Free Cash Flows (operating cash - capex)
+operating_cash = cashflow_data.loc['Total Cash From Operating Activities']
+capital_expend = cashflow_data.loc['Capital Expenditures']
+fcf = []
+for (op_cash, capex) in zip(operating_cash, capital_expend):
+  fcf.append(op_cash/1000000 - capex/1000000)
 
-    # Earnings data
-    #stacked_bar(earnings_data, dt, revenue, earnings, ('#800000','#363636'), 'Revenue/Earnings ($ millions)', './rif-logos/revenue_earnings.png')
-    
+### Save charts ###
 
-if __name__=='__main__':
-    main()
+# Financial
+price_chart(TICKER)
+bar_plots(financial_data, dt, gross, '#FEBD18', 'Gross Profit ($ millions)', './rif-logos/gross-profit.png')
+bar_plots(financial_data, dt, ebit, '#800000', 'EBIT ($ millions)', './rif-logos/ebit.png')
+# Balance sheet
+bar_plots(balance_data, dt, debt_short, '#800000', 'Short Long Term Debt ($ millions)', './rif-logos/debt_short.png')
+bar_plots(balance_data, dt, debt_long, '#FEBD18', 'Long Term Debt ($ millions)', './rif-logos/debt_long.png')
+# Cashflow
+bar_plots(cashflow_data, dt, fcf, '#FEBD18', 'Free Cash Flow ($ millions)', './rif-logos/fcf.png')
+# Earnings data
+stacked_bar(earnings_data, dt, revenue, earnings, ('#800000','#363636'), 'Revenue/Earnings ($ millions)', './rif-logos/revenue_earnings.png')
+
